17f0bf8a63c19fc4e6a7095d77230ed0f928648b
crypto49er <crypto49er@protonmail.com>
2019-02-06 17:33:24 -0500

    Updated Blotter to match Gekko Dev branch

--- plugins.js
+++ plugins.js
@@ -255,10 +255,6 @@ var plugins = [
     slug: 'blotter',
     async: false,
     modes: ['realtime'],
-    dependencies: [{
-      module: 'fs',
-      version: '0.0.1-security'
-    }]
   },
 ];
 
